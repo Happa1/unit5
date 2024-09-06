0562ba6f4141445a546318bb6608fe6a55a0b8c7
Happa1 <2025.manaha.ueda@uwcisak.jp>
2024-09-06 23:01:07 +0900

    Initial commit

---
+++ quiz/quiz_075.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+x_list=[]
+y_list=[]
+k=0
+
+print_true=True
+
+for i in range(1,1000):
+    n=i
+    while 2**k<n+k+1:
+        k+=1
+    x_list.append(n)
+    y_list.append(n/(k+n)*100)
+    if y_list[-1]>=90 and print_true==True:
+        print_true=False
+        print(f'when the length of the message is {n}, the efficiency gets bigger than 90%')
+
+plt.plot(x_list, y_list)
+plt.show()
